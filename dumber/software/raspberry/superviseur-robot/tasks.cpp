76185b4d98973d1f79484dc5135b096f4f477f5a
cla <clarisse.erpeldinger@gmail.com>
2020-04-02 12:17:36 +0200

    Ajout de la gestion du niveau de batterie -> corrections a terminer

--- dumber/software/raspberry/superviseur-robot/tasks.cpp
+++ dumber/software/raspberry/superviseur-robot/tasks.cpp
@@ -187,12 +187,33 @@ void Tasks::Run() {
  * Gestion affichage du niveau de batterie (envoi au moniteur)
  */
 void Tasks::BatteryLevel(){
-    int err;
+    int err, rs, cpMove; //modifier les variables cpMove
     rt_task_set_periodic(NULL, TM_NOW, 100000000);
     // Synchronization barrier (waiting that all tasks are starting)
     rt_sem_p(&sem_barrier, TM_INFINITE);
+    //utilisation semaphore ou cf th_move
     
     while (1) {
+        
+        //Attente du lancement de la Comm avec le robot
+        rt_task_wait_period(NULL);
+        //cout << "Periodic movement update";
+        rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE);
+        rs = robotStarted;
+        rt_mutex_release(&mutex_robotStarted);
+        if (rs == 1) {
+            rt_mutex_acquire(&mutex_move, TM_INFINITE);
+            cpMove = move;
+            rt_mutex_release(&mutex_move);
+            
+            cout << " move: " << cpMove;
+            
+            rt_mutex_acquire(&mutex_robot, TM_INFINITE);
+            robot.Write(new Message((MessageID)cpMove));
+            rt_mutex_release(&mutex_robot);
+        }
+        
+        //Envoi du message
         Message * mSent = new Message(MESSAGE_ROBOT_BATTERY_GET);
         Message * mReceived = robot.Write(mSent);
         //cout << mReceived->ToString() << endl << flush;
